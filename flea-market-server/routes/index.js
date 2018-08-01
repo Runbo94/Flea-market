1fbead83ab9ed3720d4a5fa8be98e09b3f6c40c4
Runbo Zhao <rzhao03@syr.edu>
2018-08-01 15:29:05 -0400

    1

--- flea-market-server/routes/index.js
+++ flea-market-server/routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
@@ -16,38 +16,43 @@ var router = express.Router();
 //   }
 // })
 
-const md5 = require('blueimp-md5')
-const UserModel = require('../db/models').UserModel
-const filter = {password : 0}
+const md5 = require("blueimp-md5");
+const UserModel = require("../db/models").UserModel;
+const filter = { password: 0 };
 
 //register router
-router.post('/register', function(req, res){
-  const {username, password, type} = req.body
-  UserModel.findOne({username}, function (err, user) { 
-    if(user) { 
-      res.send({code: 1, msg: 'user exists'}) 
-    } 
-    else { 
-      new UserModel({username, password: md5(password), type}).save(function (err, user) { 
-        res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7}) 
-        res.send({code: 0, data: {_id: user._id, username, type}}) 
-      })
+router.post("/register", function(req, res) {
+  const { username, password, type } = req.body;
+  UserModel.findOne({ username }, function(err, user) {
+    if (user) {
+      res.send({ code: 1, msg: "user exists" });
+    } else {
+      new UserModel({ username, password: md5(password), type }).save(function(
+        err,
+        user
+      ) {
+        console.log(user);
+        res.cookie("userid", user._id, { maxAge: 1000 * 60 * 60 * 24 * 7 });
+        res.send({ code: 0, data: { _id: user._id, username, type } });
+      });
     }
-  })
-})
+  });
+});
 
 //login router
-router.post('/login', function (req, res) {
-  const {username, password} = req.body
-  UserModel.findOne({username, password: md5(password)}, filter, function (err, user){
-    if(!user) {
-      res.send({code: 1, msg: 'username and password not match'})
-    } 
-    else { 
-      res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7}) 
-      res.send({code: 0, data: user}) 
+router.post("/login", function(req, res) {
+  const { username, password } = req.body;
+  UserModel.findOne({ username, password: md5(password) }, filter, function(
+    err,
+    user
+  ) {
+    if (!user) {
+      res.send({ code: 1, msg: "username and password not match" });
+    } else {
+      res.cookie("userid", user._id, { maxAge: 1000 * 60 * 60 * 24 * 7 });
+      res.send({ code: 0, data: user });
     }
-  })
-})
+  });
+});
 
 module.exports = router;
